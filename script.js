228f0d49be19f9a6dcac219a7f09414f660ca394
jasonc1 <jasonc1@andrew.cmu.edu>
2016-02-02 01:02:16 -0500

    added 7 counter functionalities as well as resets...still need to fix image load error

--- script.js
+++ script.js
@@ -1,43 +1,77 @@
-var canvas = document.getElementById("canvas1");
-var context = canvas.getContext("2d");
-var clicks = 0;
+var canvas = [document.getElementById("canvas0"),
+                document.getElementById("canvas1"),
+                document.getElementById("canvas2"),
+                document.getElementById("canvas3"),
+                document.getElementById("canvas4"),
+                document.getElementById("canvas5"),
+                document.getElementById("canvas6")]
+
+var context = [canvas[0].getContext("2d"),
+               canvas[1].getContext("2d"),
+               canvas[2].getContext("2d"),
+               canvas[3].getContext("2d"),
+               canvas[4].getContext("2d"),
+               canvas[5].getContext("2d"),
+               canvas[6].getContext("2d"),
+                ];
+
+var clicks = [0,0,0,0,0,0,0];
+var clicksTemp = ["clicks0", "clicks1", "clicks2", "clicks3", "clicks4", "clicks5", "clicks6"];
+var clearTemp = ["clear0", "clear1", "clear2", "clear3", "clear4", "clear5", "clear6"];
+var clearVar = ["clear0", "clear1", "clear2", "clear3", "clear4", "clear5", "clear6"];
 
 
-document.getElementById('clear').addEventListener('click', function() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    resetClick();
-    }, false);
 
-function createImageOnCanvas(imageId) {
-    canvas.style.display = "block";
+// document.getElementById('clear').addEventListener('click', function() {
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+//     resetClick();
+//     }, false);
+
+function click(i){
+    clicks[i] += 1;
+    document.getElementById(clicksTemp[i]).innerHTML = clicks[i];
+}
+function resetClick(i){
+        clicks[i] = 0;
+        document.getElementById(clicksTemp[i]).innerHTML = clicks[i];
+    }
+
+function createImageOnCanvas(imageId, i) {
+    canvas[i].style.display = "block";
         document.getElementById("images").style.overflowY = "hidden";
             var img = new Image(300,300);
     img.src = document.getElementById(imageId).src;
-    context.drawImage(img, (0), (0));
+    context[i].drawImage(img, (0), (0));
     //onload....
+
+    // document.getElementById(clearTemp[i]).addEventListener('click', function() {
+    // context[i].clearRect(0, 0, canvas[i].width, canvas[i].height);
+    // resetClick(i);
+    // }, false);
 }
 
-function draw(e) {
-    var pos = getMousePos(canvas, e);
+function clearClicks(i){
+    console.log(i);
+    console.log(context[i]);
+    context[i].clearRect(0, 0, canvas[i].width, canvas[i].height);
+    resetClick(i);
+}
+    
+
+function draw(e, i) {
+    var pos = getMousePos(canvas[i], e);
     posx = pos.x;
     posy = pos.y;
-    context.fillStyle = "red";
-    context.beginPath();
-        console.log(posx + " " + posy);
-
-    context.arc(posx, posy, 10, 0, 2*Math.PI);
-    context.fill();
-    click();
+    context[i].fillStyle = "red";
+    context[i].beginPath();
+    console.log(posx + " " + posy);
+    context[i].arc(posx, posy, 10, 0, 2*Math.PI);
+    context[i].fill();
+    click(i);
+    console.log(i);
 }
 
-function click(){
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
-}
-function resetClick(){
-        clicks = 0;
-        document.getElementById("clicks").innerHTML = clicks;
-    }
+
 
 
 function getMousePos(canvas, evt){
@@ -47,6 +81,10 @@ function getMousePos(canvas, evt){
         y: evt.clientY - rect.top
     };
 }
+
+
+
+
 
 
 window.draw = draw;
